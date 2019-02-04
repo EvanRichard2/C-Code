3a2f7dc45628ffc4ae9c0ffee055ec882d629f91
EvanRichard2 <Evan.John.Richard@gmail.com>
2019-02-04 12:06:00 -0600

    hello

---
+++ c/hello.c
@@ -0,0 +1,15 @@
+// hello.c
+// compile gcc hello.c -o hello.o
+// run ./hello
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!");
+  printf("**********************\n");
+  printf(" r  a     w      r    \n");
+  printf("   X     D    a    a  \n");
+  printf("h   a   m  b  o  n  e \n");
+  return 0;
+
+}
